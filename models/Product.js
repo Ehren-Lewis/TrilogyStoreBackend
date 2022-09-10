699f2bee4305e5370925282f059075957ef229d3
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-09-10 02:29:20 -0500

    Added all of the models, added all the model associations

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,8 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
+const Tag = require('./Tag');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,6 +12,31 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: { isDecimal: true}
+
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate :{isNumeric: true}
+    },
+    category_id : {
+        type: DataTypes.INTEGER,
+      }
   },
   {
     sequelize,
@@ -19,5 +46,16 @@ Product.init(
     modelName: 'product',
   }
 );
+
+Product.belongsTo(Category, {
+  foreignKey: "category_id", targetKey: "id"    // Good
+});
+
+Product.belongsToMany(Tag);
+
+
+
+Product.belongsToMany(Tag, {through: "ProductTag", foreignKey: "product_id"});
+Tag.belongsToMany(Product, {through: "ProductTag", foreignKey: "tag_id"});
 
 module.exports = Product;
